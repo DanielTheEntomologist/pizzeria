3e7185ac6d4d79c750dacd3103298ff5b8f91e20
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-02-17 21:54:49 +0100

    Switch active page, add files for booking functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { settings, select, classNames, templates } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initData: function () {
@@ -39,6 +40,56 @@ const app = {
     thisApp.cart = new Cart(cartElem);
   },
 
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    // get url hash
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    // find if any page is matching hash from url
+    let pageHashCorrect = false;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageHashCorrect = true;
+        break;
+      }
+    }
+
+    // if no page is matching hash from url, set first page as active
+    let id = idFromHash;
+    if (!pageHashCorrect) {
+      id = thisApp.pages[0].id;
+      window.location.hash = '#/' + id;
+    }
+
+    thisApp.activatePage(id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const clickedElement = this;
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   init: function () {
     const thisApp = this;
     console.log('*** App starting ***');
@@ -48,6 +99,7 @@ const app = {
     console.log('templates:', templates);
 
     thisApp.initData();
+    thisApp.initPages();
     thisApp.initCart();
   },
 };
